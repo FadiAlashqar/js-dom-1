b44707e13874f51d617cf9ce774b1d570f79054f
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-25 22:37:55 +0100

    [UPD] variable done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// richiamo gli elementi su cui lavorare:
+
+const immagine = document.querySelector('.img');
+const button = document.querySelector('.btn');
+const secondaImmagine = document.querySelector('.second-img');
+
