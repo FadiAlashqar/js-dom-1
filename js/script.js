d58397903a1990be441f44e9a5b3f83927f6538d
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-25 23:39:32 +0100

    [UPD] bonus done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@ const button = document.querySelector('.btn');
 
 // Corpo del programma:
 
+// button.addEventListener('click', function () {
+//     immagine.src = "./img/yellow_lamp.png"
+// });
+
+// corpo del programma bonus:
+
+const lampadinaAccesa = "./img/yellow_lamp.png";
+const lampadinaSpenta = "./img/white_lamp.png";
+
 button.addEventListener('click', function () {
-    immagine.src = "./img/yellow_lamp.png"
+   if (immagine.src.includes("white_lamp.png")) {
+    immagine.src = lampadinaAccesa;
+    button.innerText = 'Spegni'
+   }
+   else{
+    immagine.src = lampadinaSpenta;
+    button.innerText = 'accendi'
+    
+   }
 });
